2547ecabce8645854c37bbe56e366abbcca679d8
NanimonoDemonai <masa9nao5.n4.18@gmail.com>
2019-06-24 21:59:53 +0900

    nextの例

--- 1-nextJS/pages/index.tsx
+++ 1-nextJS/pages/index.tsx
@@ -1,5 +1,19 @@
 import * as React from "react";
 
 export default () => (
-    <p>Hello World</p>
+    <>
+        <h1>Hello World</h1>
+        <img src="/static/kani.png" alt="" className="kani"/>
+        <hr/>
+        <p>こんにちは世界</p>
+        <li>hoge</li>
+        <li>fuga</li>
+        <li>piyo</li>
+        { /*language=CSS*/}
+        <style jsx>{`
+            .kani {
+                max-width: 50px;
+            }
+        `}</style>
+    </>
 );
