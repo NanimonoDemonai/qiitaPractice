2fd69e2c8ba29e1baa0a6fa9704b21c71efba0f9
NanimonoDemonai <masa9nao5.n4.18@gmail.com>
2019-06-24 22:08:52 +0900

    nextの例2

--- 1-nextJS/pages/index.tsx
+++ 1-nextJS/pages/index.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
+import Head from "next/head";
 
 export default () => (
     <>
+        <Head>
+            <title>TitleはHelloWorld</title>
+        </Head>
         <h1>Hello World</h1>
-        <img src="/static/kani.png" alt="" className="kani"/>
         <hr/>
-        <p>こんにちは世界</p>
-        <li>hoge</li>
-        <li>fuga</li>
-        <li>piyo</li>
+        <p>こんにちは世界<br/>こんばんはカニ</p>
+        <img src="/static/kani.png" alt="" className="kani"/>
+
+        <p>1+1={1 + 1}</p>
+
+        {<ruby>世界<rt>World</rt></ruby>}
+
+        {[<li>hoge</li>, <li>fuga</li>, <li>piyo</li>]}
+
         { /*language=CSS*/}
         <style jsx>{`
             .kani {
