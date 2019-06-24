8242fa7e0925c55f2f23f47627b86e4500e4069a
NanimonoDemonai <masa9nao5.n4.18@gmail.com>
2019-06-24 22:29:01 +0900

    利用例

---
+++ 2-reactHook/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Layout from "../components/Layout";
+
+export default ()=>(
+    <Layout title={"カニ"}>
+        <li>hoge</li>
+        <li>fuga</li>
+        <li>piyo</li>
+    </Layout>
+)
