b9b6d02ac8a2580d393b778a06ad2df6d813603c
NanimonoDemonai <masa9nao5.n4.18@gmail.com>
2019-06-26 21:07:28 +0900

    ItemControllerを作成

--- 3-mobx/store/Item.ts
+++ 3-mobx/store/Item.ts
@@ -1,6 +1,34 @@
+import {computed, observable} from "mobx";
+
 export interface ItemModel {
     readonly name: string;//商品名
     readonly price: number;//値段
     readonly count: number;//個数
     readonly fullPrice: number;//合計
+}
+
+export class ItemController implements ItemModel {
+    readonly name: string;
+    readonly price: number;
+    @observable count: number;
+
+    constructor(name: string = "未定義", price: number = 0, count: number = 0) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    @computed get fullPrice() {
+        return this.price * this.count;
+    }
+
+
+    increment() {
+        this.count++;
+    }
+
+    decrement() {
+        if (this.count >= 0)
+            this.count--;
+    }
 }
