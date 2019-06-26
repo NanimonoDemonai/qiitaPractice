042534fdb8712771d8dec92561bc071ddecdc06e
NanimonoDemonai <masa9nao5.n4.18@gmail.com>
2019-06-26 22:13:02 +0900

    Decrementableのautorunを定義

--- 3-mobx/store/Item.ts
+++ 3-mobx/store/Item.ts
@@ -1,21 +1,41 @@
-import {computed, observable} from "mobx";
+import {autorun, computed, observable} from "mobx";
 
 export interface ItemModel {
     readonly name: string;//商品名
     readonly price: number;//値段
     readonly count: number;//個数
     readonly fullPrice: number;//合計
+    readonly decrementable: boolean;
 }
 
 export class ItemController implements ItemModel {
     readonly name: string;
     readonly price: number;
     @observable count: number;
+    @observable decrementable: boolean;
+
+    //decrementable操作のための内部Observer
+    private readonly decremantableHandler: () => void;
 
     constructor(name: string = "未定義", price: number = 0, count: number = 0) {
         this.name = name;
         this.price = price;
         this.count = count;
+        if (count < 0) {
+            this.count = 0;
+        }
+        this.decrementable = count >= 1;
+
+
+        this.decremantableHandler = autorun(() => {
+            if (this.decrementable) {
+                if (this.count <= 0)
+                    this.decrementable = false;
+            } else {
+                if (this.count >= 0)
+                    this.decrementable = true;
+            }
+        })
     }
 
     @computed get fullPrice() {
@@ -30,5 +50,10 @@ export class ItemController implements ItemModel {
     decrement() {
         if (this.count >= 0)
             this.count--;
+    }
+
+    //不要
+    dispose(){
+        this.decremantableHandler();
     }
 }
